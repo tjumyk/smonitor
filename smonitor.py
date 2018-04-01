01eb9090eea570792745d5973429e3252ce11186
tjumyk <tjumyk@gmail.com>
2018-04-02 03:06:51 +1000

    init repo

---
+++ smonitor.py
@@ -0,0 +1,102 @@
+import json
+
+import psutil
+import threading
+
+from flask import Flask, jsonify, session, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+with open('config.json') as f_config:
+    config = json.load(f_config)
+app.config['SECRET_KEY'] = config['server']['secret']
+socket_io = SocketIO(app, async_mode='gevent')
+
+clients = 0
+clients_lock = threading.Lock()
+worker_thread = None
+worker_thread_lock = threading.Lock()
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/config')
+def get_config():
+    conf = dict(config['monitor'])
+    conf['port'] = config['server']['port']
+    return jsonify(conf)
+
+
+@socket_io.on('connect')
+def socket_connect():
+    global clients, worker_thread
+    with clients_lock:
+        clients += 1
+        print('[Socket Connected] ID=%s, total clients: %d' % (request.sid, clients))
+        if worker_thread is None:
+            with worker_thread_lock:
+                worker_thread = socket_io.start_background_task(target=status_worker)
+    emit('connected', {'clients': clients})
+
+
+@socket_io.on('disconnect')
+def socket_disconnect():
+    global clients
+    with clients_lock:
+        clients -= 1
+        print('[Socket Disconnected] ID=%s, total clients: %d' % (request.sid, clients))
+
+
+@socket_io.on('ping')
+def socket_ping():
+    emit('pong')
+
+
+def status_worker():
+    global worker_thread
+    print('[Background] Worker Started')
+    while clients:
+        vm = psutil.virtual_memory()
+        sys_partition = None
+        other_partitions_total = 0
+        other_partitions_used = 0
+        for part in psutil.disk_partitions():
+            if part.fstype != 'ext4':
+                continue  # only care about regular linux partitions
+            usage = psutil.disk_usage(part.mountpoint)
+            if part.mountpoint == '/':
+                sys_partition = {'total': usage.total, 'percent': usage.percent}
+            else:
+                other_partitions_total += usage.total
+                other_partitions_used += usage.used
+        other_partitions = {
+            'total': other_partitions_total,
+            'percent': round(1000.0 * other_partitions_used / other_partitions_total) / 10.0
+        }
+        status = {
+            'cpu': {
+                'count': psutil.cpu_count(),
+                'percent': psutil.cpu_percent()
+            },
+            'memory': {
+                'total': vm.total,
+                'percent': vm.percent
+            },
+            'disk': {
+                'system': sys_partition,
+                'others': other_partitions
+            },
+            'boot_time': psutil.boot_time()
+        }
+        socket_io.emit('status', status, broadcast=True)
+        socket_io.sleep(config['monitor']['interval'])
+    with worker_thread_lock:
+        worker_thread = None
+    print('[Background] Worker Terminated')
+
+
+if __name__ == '__main__':
+    socket_io.run(app, host=config['server']['host'], port=config['server']['port'])
---
+++ static/js/app.coffee
@@ -0,0 +1,55 @@
+app = angular.module('app', [])
+
+app.controller('MainController', ['$scope', '$http', '$timeout', ($scope, $http, $timeout)->
+  human_size = (size)->
+    units = ['B', 'KB', 'MB', 'GB', 'TB']
+    unit_pos = 0
+    while size >= 1000 and unit_pos < units.length - 1
+      size /= 1024.0
+      unit_pos += 1
+    size = Math.round(size * 10) / 10
+    return "#{size}#{units[unit_pos]}"
+
+  percent_level = (percent)->
+    if percent < 80
+      return ''
+    if percent < 90
+      return 'warning'
+    return 'severe'
+
+  process_status_message = (status)->
+    status.memory.total_h = human_size(status.memory.total)
+    status.disk.system.total_h = human_size(status.disk.system.total)
+    if status.disk.others.total > 0
+      status.disk.others.total_h = human_size(status.disk.others.total)
+      status.disk.others.percent_h = status.disk.others.percent + '%'
+    else
+      status.disk.others.total_h = 'N/A'
+      status.disk.others.percent_h = 'N/A'
+    status.up_time = moment.unix(status.boot_time).toNow(true)
+
+    status.cpu.percent_level = percent_level(status.cpu.percent)
+    status.memory.percent_level = percent_level(status.memory.percent)
+    status.disk.system.percent_level = percent_level(status.disk.system.percent)
+    status.disk.others.percent_level = percent_level(status.disk.others.percent)
+
+    return status
+
+  $http.get('api/config').then (response)->
+    $scope.config = config = response.data
+    config.site_title = config.site_name + ' \u00B7 System Monitor'
+    if(config.mode == 'node')
+      local_host =
+        name: 'localhost'
+        address: 'localhost'
+      local_group =
+        name: 'Local Host'
+        hosts: [local_host]
+      config.host_groups = [local_group]
+    for group in config.host_groups
+      for host in group.hosts
+        host.socket = socket = io("//#{host.address}:#{config.port}")
+        socket.on 'status', (message)->
+          $timeout ->
+            host.status = process_status_message(message)
+])
